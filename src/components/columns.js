86235dbe78bc358b57583dd8268ae89eef6dcc83
stujlimaan <tujlimaan1999@gmail.com>
2024-03-06 17:45:19 +0530

    initial project and install react table and mockaroo json data

---
+++ src/components/columns.js
@@ -0,0 +1,33 @@
+export const COLUMNS = [
+    {
+        Header:'Id',
+        accessor:'id'
+
+    },
+    {
+        Header:'First Name',
+        accessor:'first_name'
+    }
+    ,
+    {
+        Header:'Last Name',
+        accessor:'last_name'
+    },
+    {
+        Header:'email Address',
+        accessor:'email'
+    },
+    {
+        Header:'gender',
+        accessor:'gender'
+    },
+    {
+        Header:'age',
+        accessor:'age'
+    },
+    {
+        Header:'country',
+        accessor:'country'
+    }
+]
+
